2239884eea089ce05270a87b508b7f011d212bf5
Artemius <artemcka.jdanov@yandex.ru>
2025-03-24 22:25:44 +0700

    create Layout.tsx component

---
+++ portfolio-app/src/components/Layout.tsx
@@ -0,0 +1,20 @@
+// components/layout/Layout.tsx
+import { ReactNode } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
